4569098b341a764de2faee671ce4a90c997f977d
Aravinda HB <aru03.info@gmail.com>
2021-02-04 16:47:32 +0530

    xpath

--- Eclipse_WS/JavaTraining/src/com/aim/assignments/PintDate.java
+++ Eclipse_WS/JavaTraining/src/com/aim/assignments/PintDate.java
@@ -1,5 +1,13 @@
 package com.aim.assignments;
 
+import java.util.Date;
+
 public class PintDate {
 
+	
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		System.out.println(d.toString());
+	}
 }
