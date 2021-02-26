6727cb2be50667255fc375bf2f3ab9e18da1b160
Aravinda HB <aru03.info@gmail.com>
2021-02-26 08:48:23 +0530

    TESTNG

---
+++ Eclipse_WS/actitime/src/test/java/com/skillbay/testng/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.skillbay.testng;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	
+	
+	@Test
+	public void myFirstTest()
+	{
+		System.out.println("Test-1");
+	}
+	@Test
+	public void myFirstTest1()
+	{
+		System.out.println("Test-2");
+	}
+
+}
---
+++ Eclipse_WS/actitime/src/test/java/com/skillbay/testng/TestNGHelloWorld.java
@@ -0,0 +1,11 @@
+package com.skillbay.testng;
+
+import org.testng.annotations.Test;
+
+public class TestNGHelloWorld {
+
+	@Test
+	public void f() {
+		System.out.println("MyTEst");
+	}
+}
